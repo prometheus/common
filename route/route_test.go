ee21c31a8cbad2a29cce94a8c53855e1dab92bf7
Julius Volz <julius@soundcloud.com>
2016-09-21 02:27:24 +0200

    route: Allow per-request custom context injection
    **Motivation**
    
    Currently, Prometheus's web API knows of two different contexts:
    
    - The main Prometheus server context which is passed into the web API
      and through to the query layer.
    - The per-request context which is generated by the route.Router. This
      is currently only used for storing and retrieving URL-path parameters,
      and it's not possible to influence this context at the top of the
      chain.
    
    Since merging of contexts is not a concept, we ultimately want the
    router to support a per-request context which:
    
    a) can be derived from the main server context,
    b) can be decorated with custom values/timeouts/etc. per-request
    
    The concrete motivation for b) is for Weaveworks Frankenstein to be able
    to extract multi-tenancy authentication headers and store user
    information in the context. This will then be passed on into the query
    layers and decide what data a user gets to see.
    
    Next step: update Prometheus to make use of this, get rid of the context
    in the web API itself. This will make the API package reusable by
    Frankenstein.

--- route/route_test.go
+++ route/route_test.go
@@ -4,10 +4,12 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"golang.org/x/net/context"
 )
 
 func TestRedirect(t *testing.T) {
-	router := New().WithPrefix("/test/prefix")
+	router := New(nil).WithPrefix("/test/prefix")
 	w := httptest.NewRecorder()
 	r, err := http.NewRequest("GET", "http://localhost:9090/foo", nil)
 	if err != nil {
@@ -24,4 +26,24 @@ func TestRedirect(t *testing.T) {
 	if want != got {
 		t.Fatalf("Unexpected redirect location: got %s, want %s", got, want)
 	}
+}
+
+func TestContextFn(t *testing.T) {
+	router := New(func(r *http.Request) context.Context {
+		return context.WithValue(context.Background(), "testkey", "testvalue")
+	})
+
+	router.Get("/test", func(w http.ResponseWriter, r *http.Request) {
+		want := "testvalue"
+		got := Context(r).Value("testkey")
+		if want != got {
+			t.Fatalf("Unexpected context value: want %q, got %q", want, got)
+		}
+	})
+
+	r, err := http.NewRequest("GET", "http://localhost:9090/test", nil)
+	if err != nil {
+		t.Fatalf("Error building test request: %s", err)
+	}
+	router.ServeHTTP(nil, r)
 }
