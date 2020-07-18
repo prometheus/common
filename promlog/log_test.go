9bf407d1a88a812a924da710ad30ae1ef39c12c5
Julien Pivotto <roidelapluie@inuits.eu>
2020-07-18 22:35:01 +0200

    logger: add dynamic logger
    Add the required code to make logger level dynamic in prometheus.
    
    Signed-off-by: Julien Pivotto <roidelapluie@inuits.eu>

--- promlog/log_test.go
+++ promlog/log_test.go
@@ -14,7 +14,11 @@
 package promlog
 
 import (
+	"fmt"
 	"testing"
+
+	"github.com/go-kit/kit/log/level"
+	"gopkg.in/yaml.v2"
 )
 
 // Make sure creating and using a logger with an empty configuration doesn't
@@ -24,5 +28,100 @@ func TestDefaultConfig(t *testing.T) {
 
 	if err := logger.Log("hello", "world"); err != nil {
 		t.Fatal(err)
+	}
+}
+
+func TestUnmarshallLevel(t *testing.T) {
+	l := &AllowedLevel{}
+	err := yaml.Unmarshal([]byte(`debug`), l)
+	if err != nil {
+		t.Error(err)
+	}
+	if l.s != "debug" {
+		t.Errorf("expected %s, got %s", "debug", l.s)
+	}
+}
+
+func TestUnmarshallEmptyLevel(t *testing.T) {
+	l := &AllowedLevel{}
+	err := yaml.Unmarshal([]byte(``), l)
+	if err != nil {
+		t.Error(err)
+	}
+	if l.s != "" {
+		t.Errorf("expected empty level, got %s", l.s)
+	}
+}
+
+func TestUnmarshallBadLevel(t *testing.T) {
+	l := &AllowedLevel{}
+	err := yaml.Unmarshal([]byte(`debugg`), l)
+	if err == nil {
+		t.Error("expected error")
+	}
+	expErr := `unrecognized log level "debugg"`
+	if err.Error() != expErr {
+		t.Errorf("expected error %s, got %s", expErr, err.Error())
+	}
+	if l.s != "" {
+		t.Errorf("expected empty level, got %s", l.s)
+	}
+}
+
+type recordKeyvalLogger struct {
+	count int
+}
+
+func (r *recordKeyvalLogger) Log(keyvals ...interface{}) error {
+	for _, v := range keyvals {
+		if fmt.Sprintf("%v", v) == "Log level changed" {
+			return nil
+		}
+	}
+	r.count++
+	return nil
+}
+
+func TestDynamic(t *testing.T) {
+	logger := NewDynamic(&Config{})
+
+	debugLevel := &AllowedLevel{}
+	if err := debugLevel.Set("debug"); err != nil {
+		t.Fatal(err)
+	}
+	infoLevel := &AllowedLevel{}
+	if err := infoLevel.Set("info"); err != nil {
+		t.Fatal(err)
+	}
+
+	recorder := &recordKeyvalLogger{}
+	logger.base = recorder
+	logger.SetLevel(debugLevel)
+	if err := level.Debug(logger).Log("hello", "world"); err != nil {
+		t.Fatal(err)
+	}
+	if recorder.count != 1 {
+		t.Fatal("log not found")
+	}
+
+	recorder.count = 0
+	logger.SetLevel(infoLevel)
+	if err := level.Debug(logger).Log("hello", "world"); err != nil {
+		t.Fatal(err)
+	}
+	if recorder.count != 0 {
+		t.Fatal("log found")
+	}
+	if err := level.Info(logger).Log("hello", "world"); err != nil {
+		t.Fatal(err)
+	}
+	if recorder.count != 1 {
+		t.Fatal("log not found")
+	}
+	if err := level.Debug(logger).Log("hello", "world"); err != nil {
+		t.Fatal(err)
+	}
+	if recorder.count != 1 {
+		t.Fatal("extra log found")
 	}
 }
