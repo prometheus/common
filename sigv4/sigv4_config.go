3442f5db97b726989babafee4a9ef01062f609a3
Nicole Wren <wrenn@squareup.com>
2023-06-13 17:01:59 -0400

    Add Sigv4 Endpoint config parameter
    Signed-off-by: Nicole Wren <wrenn@squareup.com>

--- sigv4/sigv4_config.go
+++ sigv4/sigv4_config.go
@@ -28,6 +28,7 @@ type SigV4Config struct {
 	SecretKey config.Secret `yaml:"secret_key,omitempty"`
 	Profile   string        `yaml:"profile,omitempty"`
 	RoleARN   string        `yaml:"role_arn,omitempty"`
+	Endpoint  string        `yaml:"endpoint,omitempty"`
 }
 
 func (c *SigV4Config) Validate() error {
