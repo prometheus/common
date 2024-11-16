4052576a2a1af101bd6316f2cdb501129f36da9e
Matthieu MOREL <matthieu.morel35@gmail.com>
2024-11-16 10:15:34 +0100

    Update version/info.go
    Co-authored-by: Ben Kochie <superq@gmail.com>
    Signed-off-by: Matthieu MOREL <matthieu.morel35@gmail.com>

--- version/info.go
+++ version/info.go
@@ -95,7 +95,7 @@ func PrometheusUserAgent() string {
 }
 
 func ComponentUserAgent(component string) string {
-	return fmt.Sprintf("%s/%s", component, Version)
+	return component+"/"+Version
 }
 
 func init() {
